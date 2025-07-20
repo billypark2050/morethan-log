f0aba63dac2c69d6fe034ae0195732ea9472a0d1
billypark <billypark@gm.gist.ac.kr>
2025-07-20 16:05:23 +0900

    update orig

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -1,20 +1,20 @@
 import styled from "@emotion/styled"
 import Link from "next/link"
 
-// const NavBar: React.FC = () => {
-//   const links = [{ id: 1, name: "About", to: "/about" }]
-//   return (
-//     <StyledWrapper className="">
-//       <ul>
-//         {links.map((link) => (
-//           <li key={link.id}>
-//             <Link href={link.to}>{link.name}</Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </StyledWrapper>
-//   )
-// }
+const NavBar: React.FC = () => {
+  const links = [{ id: 1, name: "About", to: "/about" }]
+  return (
+    <StyledWrapper className="">
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link href={link.to}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </StyledWrapper>
+  )
+}
 
 export default NavBar
 
